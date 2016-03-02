e8bb57795f707213a62680ddf3ec8da3a65c74e9
Andrew Olson <olson@cshl.edu>
2016-03-02 17:16:32 -0500

    add taxon_id filter option, assume core db

--- getBed.js
+++ getBed.js
@@ -1,37 +1,55 @@
 #!/usr/env node
 var argv = require('minimist')(process.argv.slice(2));
 
-var url = argv.swagger || 'http://brie:10050/swagger';
+var url = argv.swagger || 'http://data.gramene.org/v50/swagger';
 var feature = argv.feature || 'gene';
 var combiner = argv.combiner || 'canonical';
-var idFile = argv.ids || '/dev/fd/0';
+var idFile = argv.ids;
 var batchSize = argv.batchSize || 1000;
 
 global.gramene = {defaultServer: url};
 var gramene = require('./src/grameneSwaggerClient');
-
-var ids = [];
-require('readline').createInterface({
-  input: require('fs').createReadStream(idFile),
-  terminal: false
-})
-.on('line', function(line) {
-  ids.push(line);
-})
-.on('close', function() {
-  gramene.then(function(client) {
-    for(var i=0; i<ids.length; i+=batchSize) {
-      client['Data access'].genes(
-        {
+if (idFile) {
+  var ids = [];
+  require('readline').createInterface({
+    input: require('fs').createReadStream(idFile),
+    terminal: false
+  })
+  .on('line', function(line) {
+    ids.push(line);
+  })
+  .on('close', function() {
+    gramene.then(function(client) {
+      for(var i=0; i<ids.length; i+=batchSize) {
+        var params = {
           rows: batchSize,
           wt: 'bed',
           bedFeature: feature,
           bedCombiner: combiner,
           idList: ids.slice(i, i+batchSize)
-        }, function(res) {
-          console.log(res.data);
+        };
+        if (argv.taxon_id) {
+          params.taxon_id = argv.taxon_id;
         }
-      )
-    }
+        client['Data access'].genes(params, function(res) {
+          console.log(res.data);
+        })
+      }
+    })
   })
-})
+}
+else if (argv.taxon_id) {
+  gramene.then(function(client) {
+    client['Data access'].genes(
+      {
+        wt: 'bed',
+        bedFeature: feature,
+        bedCombiner: combiner,
+        taxon_id: argv.taxon_id,
+        db_type: 'core'
+      }, function(res) {
+        console.log(res.data);
+      }
+    )
+  })
+}
